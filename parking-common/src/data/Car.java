ff93b06d7b616f90e26ed975823b645f9212f456
alaa-barazi <brazialaa@gmail.com>
2025-07-02 19:35:31 +0300

    fixed order

--- parking-common/src/data/Car.java
+++ parking-common/src/data/Car.java
@@ -2,7 +2,7 @@ package data;
 
 import java.io.Serializable;
 import java.time.Year;
-//
+
 public class Car implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String carNumber,model;
